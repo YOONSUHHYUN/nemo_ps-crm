90b01983d1aa3c16b5d5e127c53fb073e2a7491e
윤서현 <wwwnw@naver.com>
2024-02-06 13:54:12 +0900

    title

--- main.php
+++ main.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html>
-<link rel="icon" href="./favicon.ico">
   <head>
+    <link rel="icon" href="./favicon.ico">
+    <title>네모 PS-CRM</title>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
